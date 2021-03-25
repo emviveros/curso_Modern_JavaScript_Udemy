d306590cabfa21b136f7b25bca0bab3f5ce3916d
Esteban Viveros <emviveros@users.noreply.github.com>
2021-03-25 16:23:53 -0300

    7. Variables, Constants & Comments

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,2 +1,20 @@
-console.log(1)
-console.log('hshshshs')
+// Declaração de variável apenas na primeira vez
+let age = 12;
+let year = 2021;
+
+console.log(age, year);
+
+age = 30;
+console.log(age);
+
+const points = 100;
+// points = 50;
+console.log(points);
+
+//velho modo de declarar variáveis em JavaScript
+var score = 75;
+console.log(score);
+
+/*variáveis não podem começar com números
+ palavras reservadas: https://www.w3schools.com/js/js_reserved.asp
+ */
