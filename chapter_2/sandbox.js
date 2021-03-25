7c2e253fa147589ed7eea897cf0cf36ea69c94d0
Esteban Viveros <emviveros@users.noreply.github.com>
2021-03-25 16:57:48 -0300

    Seção 2: 11 - Numbers

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,13 +1,49 @@
-// common string methods
+let radius = 10;
+const pi = 3.1416;
 
-let email = 'mario@ninja.bros';
+// console.log(radius, pi);
 
-// let result = email.lastIndexOf('n');
-// let result = email.slice(0, 5);
-// let result = email.substr(2, 10);
-// let result = email.replace('m', 'w');
-let result = email.replace('a', 'u');
+/////////////////////////////////////////////
+// operadores matemáticos +, -, *, /, **, %
+
+// console.log(10/2);
+// let result = radius % 3;
+// let result = pi * radius**2;
+
+// order of operation B I D M A S
+// B = brakets
+//
+// D = division
+// M = multiplications
+// A = adition
+// S = subtraction
+
+// let result = 5 * (10-3)**2
+
+// console.log(result);
+
+let likes = 10;
+
+// likes = likes + 1;
+// likes++;
+// likes--;
+
+// likes += 10;
+// likes -= 5;
+// likes *= 2;
+// likes /= 2;
 
 
+// console.log(likes);
+
+///////////////////////
+// NaN - not a number
+
+// console.log(5 / 'hello');
+// console.log(5 * 'hello')
+
+
+// concatenate numbers
+
+let result = 'the blog has ' + likes + ' likes';
 console.log(result);
-
