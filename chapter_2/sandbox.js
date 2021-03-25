0fbd985c46e548287d65c040ea5dc44b75782873
Esteban Viveros <emviveros@users.noreply.github.com>
2021-03-25 17:09:05 -0300

    Seção 2: 12 Template Strings

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,49 +1,20 @@
-let radius = 10;
-const pi = 3.1416;
+// template strings
+const title = 'Best reads of 1920';
+const author = 'Mario';
+const likes = 30;
 
-// console.log(radius, pi);
-
-/////////////////////////////////////////////
-// operadores matemáticos +, -, *, /, **, %
-
-// console.log(10/2);
-// let result = radius % 3;
-// let result = pi * radius**2;
-
-// order of operation B I D M A S
-// B = brakets
-//
-// D = division
-// M = multiplications
-// A = adition
-// S = subtraction
-
-// let result = 5 * (10-3)**2
-
+//concatenation way
+// let result = 'The blog called ' + title + ' by ' + author + ' has ' + likes + ' likes';
 // console.log(result);
 
-let likes = 10;
-
-// likes = likes + 1;
-// likes++;
-// likes--;
-
-// likes += 10;
-// likes -= 5;
-// likes *= 2;
-// likes /= 2;
-
-
-// console.log(likes);
-
-///////////////////////
-// NaN - not a number
-
-// console.log(5 / 'hello');
-// console.log(5 * 'hello')
-
-
-// concatenate numbers
-
-let result = 'the blog has ' + likes + ' likes';
+// template way
+let = result = `The blog called ${title} by ${author} has ${likes} likes`;
 console.log(result);
+
+// creating html templates
+let html = `
+    <h2>${title}</h2>
+    <p>By ${author}</p>
+    <span>This blog has ${likes} likes</span>
+`;
+console.log(html);
