483ee0621403654e050ba5e49ecc084527a80f87
Esteban Viveros <emviveros@users.noreply.github.com>
2021-03-25 17:50:15 -0300

    Seção 2: 16. Loose vs Strict Comparison

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,32 +1,15 @@
-// booleans & comparisons
-console.log(true, false, "true", "false");
-
-// methods car return booleans
-let email = 'luigi@ninja.bros';
-let names = ['mario', 'luigi', 'toad'];
-
-// // let result = email.includes('@')
-// let result = names.includes('luigi')
-
-// console.log(result)
-
-
-// comparison operators
 let age = 25;
 
+//loose comparison (different types can still be equal)
+
 // console.log(age == 25);
-// console.log(age == 32);
-// console.log(age != 32);
+// console.log(age == '25');
 // console.log(age != 25);
-// console.log(age > 20);
-// console.log(age < 20);
-// console.log(age >= 25);
-// console.log(age <= 25);
+// console.log(age != 25);
 
-let Name = 'shaun';
+// strict comparison (different types cannot be equal)
 
-console.log(Name == 'shaun');
-console.log(Name == 'Shaun');
-console.log(Name > 'crystal');
-console.log(Name > 'Shaun');
-console.log(Name > 'Crystal');
+console.log(age === 25);
+console.log(age === '25');
+console.log(age !== 25);
+console.log(age !== '25');
