4c8d207c42a95bb8937ae245d986a211741d5f04
Esteban Viveros <emviveros@users.noreply.github.com>
2021-03-25 17:26:57 -0300

    Seção 2: 14. Null & Undefined

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,24 +1,4 @@
-let ninjas = ['shaun', 'ryu', 'chun-li'];
+// let age;
+let age = null;
 
-// console.log(ninjas);
-// console.log(ninjas[1]);
-
-// ninjas[1] = 'ken';
-// console.log(ninjas);
-
-// let ages = [20,25,30,35];
-// console.log(ages[2]);
-
-// let random = ['shaun', 'crystal', 30, 20];
-// console.log(random);
-
-// console.log(ninjas.length);
-
-// array methods
-// let result = ninjas.join(', ');
-// let result = ninjas.indexOf('chun-li');
-// let result = ninjas.concat('ken', 'crystal');
-let result = ninjas.push('ken'); console.log(ninjas);
-result = ninjas.pop(); console.log(ninjas);
-
-console.log(result);
+console.log(age, age+3, `the age is ${age}`)
