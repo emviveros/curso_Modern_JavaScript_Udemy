5228ad095ce3c4fad41fad71ba2583f7278d26f3
Esteban Viveros <emviveros@users.noreply.github.com>
2021-03-31 22:46:11 -0300

    Chapter 3 - 24. Logical Operators

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -1,11 +1,14 @@
-// else is statement
+// logical operators - OR || and AND &&
 
-const senha = 'p@ss1234567890'
+// const senha = 'p@ss'
+// const senha = 'p@ss12334567890'
+// const sesnha = 'pass12334567890'
+const senha = 'p@ss12'
 
-if(senha.length >= 12){
+if(senha.length >= 12 && senha.includes('@')){
     console.log('Essa senha é muito forte!')
-} else if(senha.length >= 8){
-    console.log("Essa senha é longa o suficiente!");
+} else if(senha.length >= 8 || senha.includes('@') && senha.length > 5){
+    console.log("Essa senha é forte o suficiente");
 } else{
-    console.log('A senha não é longa suficiente');
+    console.log('A senha não é forte suficiente');
 }
