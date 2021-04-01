09217b8f647ae5b8267bcb031b28e3dcadd79144
Esteban Viveros <emviveros@users.noreply.github.com>
2021-03-31 22:50:23 -0300

    Chapter 3 - 25. Logical NOT

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -1,14 +1,10 @@
-// logical operators - OR || and AND &&
+// logical NOT (!)
 
-// const senha = 'p@ss'
-// const senha = 'p@ss12334567890'
-// const sesnha = 'pass12334567890'
-const senha = 'p@ss12'
+let user = false;
 
-if(senha.length >= 12 && senha.includes('@')){
-    console.log('Essa senha é muito forte!')
-} else if(senha.length >= 8 || senha.includes('@') && senha.length > 5){
-    console.log("Essa senha é forte o suficiente");
-} else{
-    console.log('A senha não é forte suficiente');
+if(!user){
+    console.log('user = ', user)
 }
+
+// console.log(!true);
+// console.log(!false);
