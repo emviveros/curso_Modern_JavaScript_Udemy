c46448be68340965ac2fe9a6dc7bb08b0a6bf9e6
Esteban Viveros <emviveros@users.noreply.github.com>
2021-03-31 22:40:21 -0300

    Chapter 3 - 23. Else & Else If

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -1,19 +1,11 @@
-// if statement
+// else is statement
 
-// const age = 24;
+const senha = 'p@ss1234567890'
 
-// if(age > 20){
-//     console.log('you are over 20 years old');
-// }
-
-// const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi'];
-
-// if(ninjas.length > 3){
-//     console.log("that's a lot of ninjas")
-// }
-
-const senha = 'pass'
-
-if(senha.length >= 8){
+if(senha.length >= 12){
+    console.log('Essa senha é muito forte!')
+} else if(senha.length >= 8){
     console.log("Essa senha é longa o suficiente!");
+} else{
+    console.log('A senha não é longa suficiente');
 }
