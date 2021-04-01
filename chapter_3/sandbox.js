23aa31695d7056fb38cff4bcae5ac4e6c1e96659
Esteban Viveros <emviveros@users.noreply.github.com>
2021-03-31 22:34:29 -0300

    Chapter 3 - 22. If Statements

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -1,8 +1,19 @@
-// do while loops
+// if statement
 
-let i=3;
+// const age = 24;
 
-do{
-    console.log('val of i is: ', i);
-    i++;
-}   while(i<5);
+// if(age > 20){
+//     console.log('you are over 20 years old');
+// }
+
+// const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi'];
+
+// if(ninjas.length > 3){
+//     console.log("that's a lot of ninjas")
+// }
+
+const senha = 'pass'
+
+if(senha.length >= 8){
+    console.log("Essa senha é longa o suficiente!");
+}
