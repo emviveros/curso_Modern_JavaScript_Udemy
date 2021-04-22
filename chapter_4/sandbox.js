aeedb89d9537d18ae8201e8ab3593cd67ddfa994
Esteban Viveros <emviveros@users.noreply.github.com>
2021-04-22 14:51:14 -0300

    Seção 4 - 33. Returning Values

--- chapter_4/sandbox.js
+++ chapter_4/sandbox.js
@@ -1,10 +1,28 @@
-// arguments & parameters
+// returning values
 
-const speak = function(name = 'luigi', time = 'night'){
-    console.log(`good ${time} ${name}!`);
+// const speak = function(name = 'luigi', time = 'night'){
+//     console.log(`good ${time} ${name}!`);
+// };
+
+// const calcArea = function(radius){
+//     let area = 3.14 * radius**2;
+//     console.log(area);
+// };
+
+// calcArea(5);
+
+
+const calcArea = function(radius){
+    let area = 3.14 * radius**2;
+    return area;
 };
 
-speak('mario', 'morning');
-speak();
-speak('shaun', 'day');
+const area = calcArea(5);
+console.log(area);
 
+const calcVol = function(area){
+    // calculo do volume em função da área
+};
+
+// podemos obter o volume usando:
+calcVol(area);
