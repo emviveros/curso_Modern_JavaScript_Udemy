d41218fc9a268d3b7a029debc01ff30e61e89172
Esteban Viveros <emviveros@users.noreply.github.com>
2021-04-23 12:37:33 -0300

    final

--- chapter_4/sandbox.js
+++ chapter_4/sandbox.js
@@ -1,44 +1,14 @@
-// regular function
-// const calcArea = function(radius){
-//      return 3.14 * radius**2;
-// };
+const name = 'shaun';
 
-// arrow function
+// functions
 
-// // se tiver apenas um parâmetro não precisa dos parênteses em (radius)
-// const calcArea = (radius) => {
-//      return 3.14 * radius**2;
-// };
+const greet = () => 'hello';
 
-// outra forma de escrever arrow function (se tiver um return simples de uma linha):
-const calcArea = radius => 3.14 * radius**2;
+let resultOne = greet();
+console.log(resultOne);
 
 
+// methods
 
-const area = calcArea(5);
-console.log('area is: ', area);
-
-// fazendo arrow function de funções usadas anteriormente:
-const greet = () => 'hello world!';
-
-console.log(greet());
-
-// outra função do tutorial Udemy
-
-// const bill = function(products, tax){
-//      let total = 0;
-//      for(let i=0; i < products.length; i++){
-//           total += products[i] + products[i] * tax;
-//      }
-//      return total;
-// }
-
-const bill = (products, tax) => {
-     let total = 0;
-     for(let i=0; i < products.length; i++){
-          total += products[i] + products[i] * tax;
-     }
-     return total;
-}
-
-console.log(bill([10,15,30], 0.2));
+let resultTwo = name.toUpperCase();
+console.log(resultTwo);
