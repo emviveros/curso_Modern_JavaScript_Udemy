7085f9e0092f7b0e60f901d97574cb7f9a39c9cb
Esteban Viveros <emviveros@users.noreply.github.com>
2021-04-24 15:20:00 -0300

    inicio

--- chapter_4/sandbox.js
+++ chapter_4/sandbox.js
@@ -6,13 +6,12 @@ let user = {
     email: 'crystal@email.com',
     location: 'berlin',
     blogs: ['why mac & cheese rules', '10 things to make with marmite'],
-    login: function(){
+    login(){
         console.log('the user logged in');
     },
-    logout: function(){
+    logout(){
         console.log('the user logged out');
     },
-    //outra forma de escrever regular functions:
     logBlogs(){
         console.log(this);
         // console.log(this.blogs);
@@ -20,16 +19,6 @@ let user = {
             console.log(blog);
         })
     },
-    logThis: () => {
-        console.log(this);
-    }
 };
 
 user.logBlogs();
-console.log(this);
-
-// quando usamos arrow function em JavaScript, o parâmetro this
-// funciona de forma diferente quando usamos regular fuctions...
-// repare que em regular functions aponta para o objeto e em 
-// arrow functions aponta para o documento principal.
-user.logThis();
