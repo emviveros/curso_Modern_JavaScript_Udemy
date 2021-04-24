5d245e7a83dec4662a894a332aa13fcf033b0045
Esteban Viveros <emviveros@users.noreply.github.com>
2021-04-24 14:52:51 -0300

    fim

--- chapter_4/sandbox.js
+++ chapter_4/sandbox.js
@@ -5,6 +5,20 @@ let user = {
     age: 30,
     email: 'crystal@email.com',
     location: 'berlin',
-    blogs: ['why mac & cheese rules', '10 things to make with marmite']
+    blogs: ['why mac & cheese rules', '10 things to make with marmite'],
+    login: function(){
+        console.log('the user logged in');
+    },
+    logout: function(){
+        console.log('the user logged out');
+    },
+    logBlogs: function(){
+        
+    }
 };
 
+user.login();
+user.logout();
+
+const name = 'mario';
+name.toUpperCase();
