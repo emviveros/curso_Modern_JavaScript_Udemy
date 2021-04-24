90e765d62ad54575c01d5f4000c840d0f5980572
Esteban Viveros <emviveros@users.noreply.github.com>
2021-04-24 14:30:46 -0300

    fim

--- chapter_4/sandbox.js
+++ chapter_4/sandbox.js
@@ -1,5 +1,14 @@
 // get a reference to the 'ul'
-const ul = document.querySelector('.people');
+const ul = document.querySelector('.people');   // tem a ver com a mágica final
 
 const people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
 
+let html = ``;
+
+people.forEach(person => {
+    // create html template
+    html += `<li style="color: purple">${person}</li>`;
+});
+
+console.log(html);
+ul.innerHTML = html;    // no momento é mágica!
