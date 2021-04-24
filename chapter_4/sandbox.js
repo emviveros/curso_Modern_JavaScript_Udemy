f788dee5e16dd509037dfc29e0fad176b1d7a82d
Esteban Viveros <emviveros@users.noreply.github.com>
2021-04-24 14:20:56 -0300

    inicio

--- chapter_4/sandbox.js
+++ chapter_4/sandbox.js
@@ -1,44 +1,5 @@
-// callbacks & foreach
+// get a reference to the 'ul'
+const ul = document.querySelector('.people');
 
-let people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+const people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
 
-people.forEach(function(){
-    console.log('something');
-});
-
-///////////////////////////////////////////////////////////
-console.log("---");
-
-people.forEach(function(person){
-    console.log(person);
-});
-
-///////////////////////////////////////////////////////////
-console.log("---");
-
-people.forEach(person => {
-    console.log(person);
-});
-
-///////////////////////////////////////////////////////////
-console.log("---");
-
-people.forEach((person, index) => {
-    console.log(index, person);
-});
-
-///////////////////////////////////////////////////////////
-console.log("---");
-
-const logPerson = (person, index) => {
-    console.log(`${index} - hello ${person}`);
-};
-
-people.forEach(logPerson);
-
-///////////////////////////////////////////////////////////
-
-// Funções de callback são funções normais, que são passadas
-// a outras funções como argumento
-
-///////////////////////////////////////////////////////////
