1700fa150d099a6e9b3b82e79d8ba7a35f205d51
Esteban Viveros <emviveros@users.noreply.github.com>
2021-04-26 17:01:48 -0300

    inicio2

--- chapter_5/sandbox.js
+++ chapter_5/sandbox.js
@@ -1,1 +1,16 @@
 // Primitive Typed & Reference Types
+// https://www.udemy.com/course/modern-javascript-from-novice-to-ninja/learn/lecture/14174797#content
+
+// primitive values
+
+let scoreOne = 50;
+let scoreTwo = scoreOne;
+
+console.log(`scoreOne: ${scoreOne}`,`scoreTwo: ${scoreTwo}`);
+
+// reference calues
+
+// userOne = { name: 'ryu', age: 30 };
+// userTwo = userOne;
+
+// console.log(userOne, userTwo);
