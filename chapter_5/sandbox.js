5bf875714c09452493bcf89323b65ecfe5f13db9
Esteban Viveros <emviveros@users.noreply.github.com>
2021-04-26 16:49:39 -0300

    fim

--- chapter_5/sandbox.js
+++ chapter_5/sandbox.js
@@ -1,1 +1,30 @@
 // Math object
+
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.E);
+
+console.log('---------------')
+
+let area = 7.1;
+console.log(Math.round(area));
+console.log(Math.floor(area));
+console.log(Math.ceil(area));
+
+console.log('---------------')
+
+area = 7.8;
+console.log(Math.round(area));
+console.log(Math.floor(area));
+console.log(Math.ceil(area));
+console.log(Math.trunc(area));
+
+console.log('---------------')
+
+//random numbers
+const random = Math.random();
+
+console.log(random);
+console.log(Math.round(random));
+
+console.log(Math.round(random * 100));
