9516c0b73781fe563ca5ea9c53b754e44470be6c
Esteban Viveros <emviveros@users.noreply.github.com>
2021-04-26 16:37:08 -0300

    seila

--- chapter_5/sandbox.js
+++ chapter_5/sandbox.js
@@ -1,11 +1,21 @@
 // object literals
 
+// const blogs = [
+//     {title: 'why mac & cheese rules', likes: 30},
+//     {title: '10 things to make with marmite', likes: 50}
+// ];
+
+// console.log(blogs);
+
 let user = {
     name: 'crystal',
     age: 30,
     email: 'crystal@email.com',
     location: 'berlin',
-    blogs: ['why mac & cheese rules', '10 things to make with marmite'],
+    blogs: [
+        {title: 'why mac & cheese rules', likes: 30},
+        {title: '10 things to make with marmite', likes: 50}
+    ],
     login(){
         console.log('the user logged in');
     },
@@ -13,10 +23,11 @@ let user = {
         console.log('the user logged out');
     },
     logBlogs(){
-        console.log(this);
+        // console.log(this);
         // console.log(this.blogs);
+        console.log('this user has written the following blogs:');
         this.blogs.forEach(blog => {
-            console.log(blog);
+            console.log(blog.title, blog.likes);
         })
     },
 };
