2ad5769c21db47b7a1a4b6a5d07129f0364f9b94
Esteban Viveros <emviveros@users.noreply.github.com>
2021-04-27 17:08:19 -0300

    inicio DOM

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -1,1 +1,1 @@
-// https://www.udemy.com/course/modern-javascript-from-novice-to-ninja/learn/lecture/14174823#content
+// https://www.udemy.com/course/modern-javascript-from-novice-to-ninja/learn/lecture/14174831#content
