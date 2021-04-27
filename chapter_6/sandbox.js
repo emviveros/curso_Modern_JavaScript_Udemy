7d27a5c475b2ba10328a6ad2553cb5f3ba62efa9
Esteban Viveros <emviveros@users.noreply.github.com>
2021-04-27 17:39:23 -0300

    fim

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -3,6 +3,28 @@
 // const paragrafo = document.querySelector('p');
 // const paragrafo = document.querySelector('.error');
 // const paragrafo = document.querySelector('div.error');
-const paragrafo = document.querySelector('body > h1');
+// const paragrafo = document.querySelector('body > h1');
 
-console.log(paragrafo);
+// console.log(paragrafo);
+
+////////////////////////////////////
+
+// const paragrafos = document.querySelectorAll('p')
+
+// console.log(paragrafos);
+// console.log(paragrafos[0]);
+
+// console.log('-----------');
+
+// paragrafos.forEach(paragrafo => {
+//     console.log(paragrafo);
+// });
+
+// console.log(paragrafos[2]);
+
+///////////////////////////////////////
+
+const paragrafos = document.querySelectorAll('p');
+const errors = document.querySelectorAll('.error');
+
+console.log(errors);
