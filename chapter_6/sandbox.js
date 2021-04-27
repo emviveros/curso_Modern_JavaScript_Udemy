5c6d96eae56331337bb3722adb3f85d966c5f7d9
Esteban Viveros <emviveros@users.noreply.github.com>
2021-04-27 17:28:37 -0300

    querySelector method

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -1,2 +1,8 @@
 // https://www.udemy.com/course/modern-javascript-from-novice-to-ninja/learn/lecture/14174853#content
 
+// const paragrafo = document.querySelector('p');
+// const paragrafo = document.querySelector('.error');
+// const paragrafo = document.querySelector('div.error');
+const paragrafo = document.querySelector('body > h1');
+
+console.log(paragrafo);
