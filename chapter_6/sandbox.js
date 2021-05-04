b939e34b278383403b060ac17ea2128f0aa34876
Esteban Viveros <emviveros@users.noreply.github.com>
2021-05-04 16:44:02 -0300

    Modifica para códigos iniciais da lição

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -1,12 +1,1 @@
-const link = document.querySelector('a');
-
-console.log(link.getAttribute('href'));
-link.setAttribute('href', 'http://www.estebanviveros.com');
-link.innerText = 'Site do Esteban';
-
-const mssg = document.querySelector('p');
-
-console.log(mssg.getAttribute('class'));
-mssg.setAttribute('class', 'success');
-
-mssg.setAttribute('style', 'color: green');
+const title = document.querySelector('h1');
