0a47bb2333e11796719fbaff58695b5248663f12
Esteban Viveros <emviveros@users.noreply.github.com>
2021-05-06 17:20:55 -0300

    prepara códigos para segunda parte da lição. apagando conteúdo de sandbox.js e modificando index.html

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -1,8 +1,1 @@
-const content = document.querySelector('p');
-
-console.log(content.classList);
-content.classList.remove('error');
-console.log(content.classList);
-content.classList.add('success');
-console.log(content.classList);
 
