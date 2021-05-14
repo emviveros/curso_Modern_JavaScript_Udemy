d014d67a524d1ec79d7d6d8ae44b3548fc52c53e
Esteban Viveros <emviveros@users.noreply.github.com>
2021-05-14 18:04:24 -0300

    modifica código para iniciar lição 6.55 Event Bubbling (and delegation)

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -1,12 +1,9 @@
 const ul = document.querySelector('ul');
-// ul.remove();
-
 const button = document.querySelector('button');
+
 button.addEventListener('click', () => {
-    // ul.innerHTML += '<li>something new</li>'
     const li = document.createElement('li');
     li.textContent = 'something new to do';
-    // ul.append(li);
     ul.prepend(li);
 });
 
@@ -14,12 +11,6 @@ const items = document.querySelectorAll('li');
 
 items.forEach(item => {
     item.addEventListener('click', e => {
-
-        // e.target.style.textDecoration = "line-through";
         e.target.remove();
-
     });
 });
-
-// Não dá pra apagar os itens adicionados pelo botão add new ToDo
-// no próximo video será como fazer isso
