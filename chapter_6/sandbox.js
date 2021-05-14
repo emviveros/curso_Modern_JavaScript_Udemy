1676afe1b6d2f54e70f4cd5aef79ce33757297df
Esteban Viveros <emviveros@users.noreply.github.com>
2021-05-14 17:54:35 -0300

    adicionando elementos com método innerHTML

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -1,9 +1,18 @@
+const ul = document.querySelector('ul');
+// ul.remove();
+
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    ul.innerHTML += '<li>something new</li>'
+});
+
 const items = document.querySelectorAll('li');
 
 items.forEach(item => {
     item.addEventListener('click', e => {
 
-        e.target.style.textDecoration = "line-through";
+        // e.target.style.textDecoration = "line-through";
+        e.target.remove();
 
     });
 });
