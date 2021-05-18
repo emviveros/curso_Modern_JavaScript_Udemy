4430739f9f735e1b5b20282f797e8b1c64514894
Esteban Viveros <emviveros@users.noreply.github.com>
2021-05-18 11:26:20 -0300

    Adiciona código referente a aplicação do comportamento de apagar tag quando item é clicado (válido para tags adicionadas por botão tb). 6.55 Event Bubbling & Delegation

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -15,14 +15,12 @@ button.addEventListener('click', () => {
 //         e.stopPropagation(); // interrompe bubbling event
 //         e.target.remove();
 //     });
-// });
 
 
-// Agora depois de comentar código de adição de evento acima,
-// associamos o target ao <ul>, (parent) das tags <li>, assim
-// não importa se eu adicionei ou já estava presente, todas as <li>
-// receberão um target e podem executar eventos associados a elas.
 ul.addEventListener('click', e => {
     // console.log('event in UL');
-    console.log(e.target);
+    // console.log(e);  //Verifica-se com esta linha que cada li tem tagname = LI
+    if(e.target.tagName === 'LI'){
+        e.target.remove();
+    }
 });
