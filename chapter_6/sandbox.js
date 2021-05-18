55ca3893d436d70207d761908ac9b60d8f430831
Esteban Viveros <emviveros@users.noreply.github.com>
2021-05-18 11:15:48 -0300

    Adiciona código referente a aplicação de event target a uma tag pai para associação de eventos a todas as tags filhas. 6.55 Event Bubbling & Delegation

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -7,16 +7,22 @@ button.addEventListener('click', () => {
     ul.prepend(li);
 });
 
-const items = document.querySelectorAll('li');
+// const items = document.querySelectorAll('li');
 
-items.forEach(item => {
-    item.addEventListener('click', e => {
-        console.log('event in LI');
-        e.stopPropagation(); // interrompe bubbling event
-        e.target.remove();
-    });
-});
+// items.forEach(item => {
+//     item.addEventListener('click', e => {
+//         console.log('event in LI');
+//         e.stopPropagation(); // interrompe bubbling event
+//         e.target.remove();
+//     });
+// });
 
+
+// Agora depois de comentar código de adição de evento acima,
+// associamos o target ao <ul>, (parent) das tags <li>, assim
+// não importa se eu adicionei ou já estava presente, todas as <li>
+// receberão um target e podem executar eventos associados a elas.
 ul.addEventListener('click', e => {
-    console.log('event in UL');
+    // console.log('event in UL');
+    console.log(e.target);
 });
